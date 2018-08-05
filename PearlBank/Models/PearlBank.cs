201a026a9d33b7995cb84f66f9f2d34291d0decf
Miči <miroslava.hreskova@student.tuke.sk>
2018-08-05 19:17:41 +0200

    bank example
    inheritance
    polymorphism
    interfaces
    collections
    file manipulation

---
+++ PearlBank/Models/PearlBank.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace PearlBank.Models
+{
+    public class Bank : IBank, ISave
+    {
+        Dictionary<string, IAccount> accountDictionary = new Dictionary<string, IAccount>();
+
+        public IAccount FindAccount(string name)
+        {
+            if (accountDictionary.ContainsKey(name))
+                return accountDictionary[name];
+            else
+                return null;
+        }
+
+        public bool StoreAccount(IAccount account)
+        {
+            if (accountDictionary.ContainsKey(account.GetName()))
+                return false;
+            accountDictionary.Add(account.GetName(), account);
+            return true;
+        }
+
+        public bool Save(string filename)
+        {
+            System.IO.TextWriter textOut = null;
+            try
+            {
+                textOut = new System.IO.StreamWriter(filename);
+                Save(textOut);
+            }
+            catch { return false; }
+            finally
+            {
+                if (textOut != null)
+                {
+                    textOut.Close();
+                }
+            }
+            return true;
+        }
+
+        public void Save(System.IO.TextWriter textOut)
+        {
+            textOut.WriteLine(accountDictionary.Count);
+            foreach (Account account in accountDictionary.Values)
+            {
+                account.Save(textOut);
+            }
+        }
+
+        public static Bank Load(string fileName)
+        {
+            System.IO.TextReader textIn = new System.IO.StreamReader(fileName);
+            Bank result = new Bank();
+            string countString = textIn.ReadLine();
+            int count = int.Parse(countString);
+            for (int i = 0; i < count; i++)
+            {
+                Account account = Account.Load(textIn);
+                result.accountDictionary.Add(account.GetName(), account);
+            }
+            return result;
+        }
+    }
+}
---
+++ PearlBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using PearlBank.Models;
+using System;
+
+namespace PearlBank
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Bank ourBank = new Bank();
+            Account account = new Account("Sipkova Ruzenka", "Za 7 horami", 1000000);
+            if (ourBank.StoreAccount(account))
+                Console.WriteLine("Account added to bank");
+            Account account2 = new Account("Snehulienka", "Za 7 dolami", 100);
+            if (ourBank.StoreAccount(account2))
+                Console.WriteLine("Account added to bank");
+            ourBank.Save("Test.txt");
+
+            Bank loadBank = Bank.Load("Test.txt");
+            IAccount storedAccount = loadBank.FindAccount("Rob");
+            if (storedAccount != null)
+                Console.WriteLine("CustomerAccount found in bank");
+            storedAccount = loadBank.FindAccount("David");
+            if (storedAccount != null)
+                Console.WriteLine("BabyAccount found in bank");
+        }
+    }
+}
